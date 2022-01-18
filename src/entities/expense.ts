74355ed4605aaab9932e2caef5f4a2320507f9d2
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-18 17:40:33 -0300

    feat: ensure it will throw an error when a empty value has been passed

--- src/entities/expense.ts
+++ src/entities/expense.ts
@@ -1,10 +1,12 @@
 export default class Expense {
   id: string;
   description: string;
+  value: number;
 
   constructor(id: string, description: string, value: number, date: string) {
     this.setId(id);
     this.setDescription(description);
+    this.setValue(value);
   }
 
   setId(id: string) {
@@ -19,5 +21,12 @@ export default class Expense {
       throw new Error('Description cannot be empty');
     }
     this.description = description;
+  }
+
+  setValue(value: number) {
+    if (value === null || value === 0) {
+      throw new Error('Value cannot be empty');
+    }
+    this.value = value;
   }
 }
