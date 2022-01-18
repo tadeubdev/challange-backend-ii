50fc38fd0643522562eeaee30a1cf10d880a9282
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-18 17:39:24 -0300

    feat: ensure it will throw an error when a empty description has been passed

--- src/entities/expense.ts
+++ src/entities/expense.ts
@@ -1,8 +1,10 @@
 export default class Expense {
   id: string;
+  description: string;
 
   constructor(id: string, description: string, value: number, date: string) {
     this.setId(id);
+    this.setDescription(description);
   }
 
   setId(id: string) {
@@ -10,5 +12,12 @@ export default class Expense {
       throw new Error('Id cannot be empty');
     }
     this.id = id;
+  }
+
+  setDescription(description: string) {
+    if (description === '' || description === null) {
+      throw new Error('Description cannot be empty');
+    }
+    this.description = description;
   }
 }
