89041e71749c485a5eee23187fcfca8e3120067a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-18 17:37:43 -0300

    test: ensure it will throw an erro when a empty id has been passed

--- tests/entities/expense.spec.ts
+++ tests/entities/expense.spec.ts
@@ -2,6 +2,9 @@ import Expense from "../../src/entities/expense";
 
 describe('Expense', () => {
   it('ensure it create an instance', () => {
-    expect(new Expense()).toBeTruthy();
+    expect(new Expense('any_id', 'any_desc', 10.00, '2021-12-01 12:00:00')).toBeTruthy();
   });
+  it('ensure it throws an error when a empty id has been passed', () => {
+    expect(() => new Expense(null, 'any_desc', 10.00, '2021-12-01 12:00:00')).toThrowError();
+  })
 });
