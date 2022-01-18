ce01193e91e1cc1ff06d1c841637eb394475451a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-18 17:28:09 -0300

    refactor: change variables privacy on Recipe

--- src/entities/recipe.ts
+++ src/entities/recipe.ts
@@ -1,8 +1,8 @@
 export default class Recipe {
-  private id: string;
-  private description: string;
-  private value: number;
-  private date: string;
+  id: string;
+  description: string;
+  value: number;
+  date: string;
 
   constructor(id: string, description: string, value: number, date: string) {
     this.setId(id);
