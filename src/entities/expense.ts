fa3c53b6b5a3fb5ffa13ac4ba05175f9fd26203d
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-18 17:37:55 -0300

    feat: ensure it will throw an erro when a empty id has been passed

--- src/entities/expense.ts
+++ src/entities/expense.ts
@@ -1,2 +1,14 @@
 export default class Expense {
+  id: string;
+
+  constructor(id: string, description: string, value: number, date: string) {
+    this.setId(id);
+  }
+
+  setId(id: string) {
+    if (id === '' || id === null) {
+      throw new Error('Id cannot be empty');
+    }
+    this.id = id;
+  }
 }
