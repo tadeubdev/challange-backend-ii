acbb20a5372ffaf2ca7fe0ae08eddb6ea2913c8b
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-18 17:39:07 -0300

    test: ensure it will throw an error when a empty description has been passed

--- tests/entities/expense.spec.ts
+++ tests/entities/expense.spec.ts
@@ -7,4 +7,7 @@ describe('Expense', () => {
   it('ensure it throws an error when a empty id has been passed', () => {
     expect(() => new Expense(null, 'any_desc', 10.00, '2021-12-01 12:00:00')).toThrowError();
   })
+  it('ensure it throws an error when a empty description has been passed', () => {
+    expect(() => new Expense('any_id', null, 10.00, '2021-12-01 12:00:00')).toThrowError();
+  })
 });
