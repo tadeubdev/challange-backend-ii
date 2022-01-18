59906e2ff278ad4f04562de2a7854f5c39b50819
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-18 17:02:19 -0300

    test: ensure it will throws an error when a empty value has been passed

--- tests/entities/recipies.spec.ts
+++ tests/entities/recipies.spec.ts
@@ -2,18 +2,23 @@ import Recipe from "../../src/entities/recipe";
 
 describe('Recipies', () => {
   test('ensure it can create an object', () => {
-    const sut = new Recipe('any_id', 'any_description', 0.00, '2022-01-18 16:53:00');
+    const sut = new Recipe('any_id', 'any_description', 10.00, '2022-01-18 16:53:00');
 
     expect(sut).toBeInstanceOf(Recipe);
   });
   test('ensure it trhows an error if a empty id has been passed', () => {
     expect(() => {
-      new Recipe('', 'any_description', 0.00, '2022-01-18 16:53:00');
+      new Recipe('', 'any_description', 10.00, '2022-01-18 16:53:00');
     }).toThrowError();
   });
   test('ensure it trhows an error if a description id has been passed', () => {
     expect(() => {
-      new Recipe('any_id', '', 0.00, '2022-01-18 16:53:00');
+      new Recipe('any_id', '', 10.00, '2022-01-18 16:53:00');
+    }).toThrowError();
+  });
+  test('ensure it trhows an error if a empty value has been passed', () => {
+    expect(() => {
+      new Recipe('any_id', 'any_description', 0.00, '2022-01-18 16:53:00');
     }).toThrowError();
   });
 });
