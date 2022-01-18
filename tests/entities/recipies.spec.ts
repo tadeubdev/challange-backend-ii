287e2c0dd993fb9c3de30e3d51d655b5c5331193
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-01-18 16:57:13 -0300

    test: ensure it throws an error when a empty id has been passed

--- tests/entities/recipies.spec.ts
+++ tests/entities/recipies.spec.ts
@@ -2,8 +2,13 @@ import Recipe from "../../src/entities/recipe";
 
 describe('Recipies', () => {
   test('ensure it can create an object', () => {
-    const sut = new Recipe();
+    const sut = new Recipe('any_id', 'any_description', 0.00, '2022-01-18 16:53:00');
 
     expect(sut).toBeInstanceOf(Recipe);
+  });
+  test('ensure it trhows an error if a empty id has been passed', () => {
+    expect(() => {
+      new Recipe('', 'any_description', 0.00, '2022-01-18 16:53:00');
+    }).toThrowError();
   });
 });
